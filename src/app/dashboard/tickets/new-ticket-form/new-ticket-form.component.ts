e655a16dc124781acde60cecde97e05d52353db0
Cuong Le <cuong.lm@cs.ventures>
2024-10-26 16:31:34 +0700

    feat: control component

--- src/app/dashboard/tickets/new-ticket-form/new-ticket-form.component.ts
+++ src/app/dashboard/tickets/new-ticket-form/new-ticket-form.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ControlComponent } from "../../../shared/control/control.component";
 
 @Component({
   selector: 'app-new-ticket-form',
   standalone: true,
-  imports: [],
+  imports: [ControlComponent],
   templateUrl: './new-ticket-form.component.html',
   styleUrl: './new-ticket-form.component.css'
 })
