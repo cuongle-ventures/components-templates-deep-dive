f8ab99ce27b32ce0812c2063ba65693a78e3a21c
Cuong Le <cuong.lm@cs.ventures>
2024-10-27 11:03:06 +0700

    chore: lifecycle

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, HostBinding, HostListener, inject, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -9,9 +9,24 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
   // global styles
   encapsulation: ViewEncapsulation.None,
   host: {
-    class: 'control'
+    // class: 'control',
+    // '(click)': 'onClick()'
   }
 })
 export class ControlComponent {
+  @HostBinding('class') className = 'control';
+  @HostBinding('title') title = 'Control';
+
+  // Getting current host element
+  private el = inject(ElementRef);
+
+  @HostListener('click') onClick() {
+    console.log('clicked', this.el)
+  }
+
+  @HostListener('mouseover') onMouseOver() {
+    // console.log('mouse over')
+  }
+
   @Input({ required: true }) label!: string;
 }
